c7bb74085f6d78c8e21616804c620bf4b4e73423
BHa2R00 <32067941+BHa2R00@users.noreply.github.com>
2025-12-26 14:10:04 +0000

    Update test2.asm

--- test2.asm
+++ test2.asm
@@ -1,8 +1,10 @@
 (inc "init.asm")
-(set io_i_a0 (const #x1))
-(set io_oe_a0 (const #x1))
-(set baud (const 18))
-((c "Shit!
-urmom is so fat
-") (tx (const c)))
-(quit)
+(set io_i_a0 D=C1)
+(set io_oe_a0 D=C1)
+(set baud (const 1))
+(_loop)
+((c "Shit!") (tx (const c))) (tx (const #\Newline)) (tx (const #\Return))
+((c "urmom is so FAT") (tx (const c))) (tx (const #\Newline)) (tx (const #\Return))
+(_wait) (const #b100) io_c_a0 D=D&M _wait D?JEQ 
+_loop D?JMP
+;(quit)
