05acf92740773ed846cedca8a1a2624920d090bc
BHa2R00 <32067941+BHa2R00@users.noreply.github.com>
2025-12-26 14:11:08 +0000

    Update test2.asm

--- test2.asm
+++ test2.asm
@@ -1,7 +1,7 @@
 (inc "init.asm")
 (set io_i_a0 D=C1)
 (set io_oe_a0 D=C1)
-(set baud (const 1))
+(set baud (const 1)) ;; nop delay for 500kHz Baudrate with 50MHz main clock 
 (_loop)
 ((c "Shit!") (tx (const c))) (tx (const #\Newline)) (tx (const #\Return))
 ((c "urmom is so FAT") (tx (const c))) (tx (const #\Newline)) (tx (const #\Return))
