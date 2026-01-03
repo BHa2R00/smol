2fe85ed74113069da3e5ad14324424e0fd3b2df7
BHa2R00 <32067941+BHa2R00@users.noreply.github.com>
2026-01-03 17:24:06 +0000

    Update helloworld.asm

--- helloworld.asm
+++ helloworld.asm
@@ -2,7 +2,7 @@
 (set io_i_a0 (const 1))
 (set io_oe_a0 (const #b1))
 (set io_os_a0 (const 0))
-(set baud (const 16)) ;; delay for 115200 baudrate in 50MHz clock
+(set baud (const 59)) ;; delay for 38400 baudrate in 50MHz clock
 ((c "hello world!") (putc (const c))) 
 (putc (const #\Newline)) (putc (const #\Return))
 (quit)
