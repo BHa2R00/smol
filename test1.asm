4d434266f9a5bdf7eaadba0e653fba8c7c54ee7a
BHa2R00 <32067941+BHa2R00@users.noreply.github.com>
2026-01-03 10:28:53 +0000

    Update test1.asm

--- test1.asm
+++ test1.asm
@@ -1,28 +1,11 @@
 (inc "init.asm")
-
-(v1 1) (v2 1) (v3 1) (v4 1) (v5 1) (v6 1)
-(set v2 (<< (set v1 (1<< 6)) -3))
-(set 
-  v4 
-  (() 
-    (set v3 (0<< 5)) 
-    8 D=D^<<A))
-
-(set io_oe_a0 (const #xff))
-(((a v4) (b v5) (c v6))
-  (set a (const 1)) (set b (const 1)) (set c (const 1))
-  (_loop)
-  (set a (eval b)) (set b (eval c))
-  (set c (+ a b))
-  (set io_i_a0 (eval c))
-  (jlt _loop (eval c) 80)
-)
-((v #(1 2 3 4))
-((c "shit") (set io_i_a0 c))
- (set io_i_a0 (const v)))
-(set baud (const 5))
-(set io_i_a0 (const #x1))
-(set io_oe_a0 (const #x1))
-((c "shit") (set v1 (eval c)) (tx (const c)))
-
-(quit)
+(set io_oe_a0 (const #b1))
+(set io_os_a0 (const 0))
+(set io_ie_a0 (const #b100))
+(set io_is_a0 (const 0))
+(set baud (const 16)) ;; delay for 115200 baudrate in 50MHz clock
+(_loop)
+((c "press k1") (putc (const c))) 
+(putc (const #\Newline)) (putc (const #\Return))
+(_wait) (eval io_c_a0) _wait D?JNE
+_loop D?JMP
